2f7106a86be11eb0f75c7275c71580cedbb48c74
vj-kanagaraj <Amma@867777>
2021-02-26 15:17:38 +0530

    changing break tag with paragraph tag

--- doc_extractor/extractor/msd_processing.py
+++ doc_extractor/extractor/msd_processing.py
@@ -45,6 +45,7 @@ class msd_extraction(base):
     def text_from_html(self,file_name):             # only for MSD
         print('entering text to _html')
         html = self.docx_to_html(file_name)
+        print('html---->',html)
         soup = BeautifulSoup(html, 'html.parser')
         '''paragraphs = soup.find_all(['p', 'li'])
         for para in paragraphs:
@@ -65,59 +66,6 @@ class msd_extraction(base):
             for para in paragraphs:
                 # print('para------>',para)
                 yield para
-
-    # def heading_value_extraction(self,file_name):                       # only for MSD
-    #     tmp = []
-    #     final = {}
-    #     key = ''
-    #     generator = self.text_from_html(file_name)
-    #     paragraphs = [component for component in generator]
-    #     for i, content in enumerate(paragraphs):
-    #         text = str(content)
-    #         if '' in tmp:
-    #             tmp.remove('')
-    #         if re.findall(self.regex_heading_msd, content.text.strip()) or '<li>' in text:
-    #         # if re.findall(self.regex_heading_msd, text):
-    #         #     print(text, '----------super')
-    #             try:
-    #                 if key and (key not in final):
-    #                     if tmp:
-    #                         # final[key] = ['$$'.join(tmp)]
-    #                         yield key , ['$$'.join(tmp)]
-    #                 elif key in final:
-    #                     if tmp:
-    #                         # final[key].append('$$'.join(tmp))
-    #                         yield key ,['$$'.join(tmp)]
-    #                 key = re.sub(r'<.*?>', '', text)
-    #                 # print(key)
-    #                 tmp.clear()
-    #             except:
-    #                 pass
-    #         else:
-    #             if i == len(paragraphs) - 1:
-    #                 text = text.strip()
-    #                 tmp = [t for t in tmp if t]
-    #                 if text and not re.findall(r"Panel\s\d", text):
-    #                     text = text.replace('<strong>', '<b>').replace('</strong>', '</b>')
-    #                     text = re.sub(r"<(\/?[^/bems]).*?>", '', text)
-    #                     tmp.append(text)
-    #                 if key not in final:
-    #                     if tmp:
-    #                         # final[key] = ['$$'.join(tmp)]
-    #                         yield key, ['$$'.join(tmp)]
-    #                 elif key in final:
-    #                     if tmp:
-    #                         # final[key].append('$$'.join(tmp))
-    #                         yield key, ['$$'.join(tmp)]
-    #                 else:
-    #                     pass
-    #             else:
-    #                 text = text.strip()
-    #                 tmp = [t for t in tmp if t]
-    #                 if text and not re.findall(r"Panel\s\d", text):  # filter out heading like 'big panel 1'
-    #                     text = text.replace('<strong>', '<b>').replace('</strong>', '</b>')
-    #                     text = re.sub(r"<(\/?[^/bems]).*?>", '', text)
-    #                     tmp.append(text)
 
     def heading_value_extraction2(self,file_name):
         tmp = []
@@ -210,11 +158,14 @@ class msd_extraction(base):
                 except:
                     pass
             if prediction in msd_categories_lang_exception:
-                val = value[0].replace('$$','<br>')
+                # val = value[0].replace('$$','<br>')
+                val = ' '.join([f"<p>{_val.replace('$$','')}</p>" for _val in value[0].split('$$')])
+                cleaned_text = val.translate(str.maketrans("","",string.punctuation))
+                cleaned_text = re.sub(r'\d','',cleaned_text).lower()
                 try:
-                    lang = lang_detect(val.translate(str.maketrans("","",string.punctuation)))
+                    lang = lang_detect(cleaned_text)
                 except:
-                    lang = classify(val.translate(str.maketrans("", "", string.punctuation)))[0]
+                    lang = classify(cleaned_text)[0]
                 all_lang.add(lang)
                 if prediction in final:
                     final[prediction].append({lang: str(val)})
@@ -222,10 +173,12 @@ class msd_extraction(base):
                     final[prediction] = [{lang: str(val)}]
             else:
                 for para in value[0].split('$$'):
+                    cleaned_text = para.translate(str.maketrans("", "", string.punctuation))
+                    cleaned_text = re.sub(r'\d', '', cleaned_text).lower()
                     try:
-                        lang = lang_detect(para.translate(str.maketrans("","",string.punctuation)))
+                        lang = lang_detect(cleaned_text)
                     except:
-                        lang = classify(para.translate(str.maketrans("","",string.punctuation)))[0]
+                        lang = classify(cleaned_text)[0]
                     all_lang.add(lang)
                     if prediction in final:
                         final[prediction].append({lang: para})
@@ -242,155 +195,3 @@ class msd_extraction(base):
         # print('after validation',self.final)
         return self.final
 
-
-    # def main2(self,file_name):                      # group by language
-    #     final = {}
-    #     all_lang = set()
-    #     check = 0
-    #     self.file_name = file_name
-    #     for key, value in self.heading_value_extraction(file_name):
-    #         prediction = base('msd', msd_model_location).prediction(key)['output']
-    #         if prediction in msd_categories_lang_exception:
-    #             val = value[0].replace('$$',' ')
-    #             # lang = lang_detect(val)[0]
-    #             lang = lang_detect(str(val).lower())
-    #             all_lang.add(lang)
-    #             if prediction in final:
-    #                 final[prediction].append({lang: val})
-    #             else:
-    #                 final[prediction] = [{lang: val}]
-    #         elif prediction in ['unique_identifier', 'reg_number']:
-    #             if prediction in final:
-    #                 final[prediction].extend(value)
-    #             else:
-    #                 final[prediction] = value
-    #         else:
-    #             for val in value:
-    #                 if '$$' in val:
-    #                     print('inside dollar split ##########------>',val)
-    #                     prev_lang = None
-    #                     for index , text in enumerate(val.split('$$')):
-    #                         text = text.replace('$$',' ')
-    #                         lang = lang_detect(str(text).lower())
-    #                         all_lang.add(lang)
-    #                         if prediction in final:
-    #                              # if lang in any(lang_detail == lang for lang_content in final[prediction] for lang_detail in lang_content.keys()):
-    #                             for lang_content in final[prediction]:
-    #                                 for lang_detail in lang_content.keys():
-    #                                     if lang_detail == lang:
-    #                                         lang_content[lang] = ' '.join((lang_content[lang],text))
-    #                                         check = 1
-    #                                         break
-    #                                     else:
-    #                                         pass
-    #                             if check == 0:
-    #                                 final[prediction].append({lang: text})
-    #                             else:
-    #                                 check = 0
-    #                         else:
-    #                             print(text,'---->first')
-    #                             final[prediction] = [{lang: text}]
-    #                 else:
-    #                     print('inside not dollar split ##########------>',val)
-    #                     lang = lang_detect(str(value[0]).lower())
-    #                     all_lang.add(lang)
-    #                     if prediction in final:
-    #                         final[prediction].append({lang: val})
-    #                     else:
-    #                         final[prediction] = [{lang: val}]
-    #     # if 'unique_identifier' in final:
-    #     #     print('checking unique identifiers')
-    #     #     print(final['unique_identifier'])
-    #     #     for key, identifier in self.regex_parsers_generator(final['unique_identifier']):
-    #     #         identifier = str(identifier).strip()
-    #     #         print('identifier--->',identifier)
-    #     #         if identifier:
-    #     #             final[key] = [identifier]
-    #     #     final.pop('unique_identifier',None)
-    #     if 'unique_identifier' in final:
-    #         identifier_clear = []
-    #         for identifier in final['unique_identifier']:
-    #             identifier_clear.append(identifier.replace('$$',' '))
-    #         final['unique_identifier'] = identifier_clear
-    #     if 'None' in final:
-    #         final.pop('None',None)
-    #     final = {**{'status': 1,'language': list(all_lang),'file_name': [file_name]},**final}
-    #     self.validation(final)
-    #     return final
-
-    # def main_old(self,file_name):
-    #     final = {}
-    #     all_lang = set()
-    #     for key , value in self.heading_value_extraction2(file_name):
-    #         prediction = base('msd',msd_model_location).prediction(key)['output']
-    #         print(value[0])
-    #         if prediction in msd_categories_lang_exception:
-    #             val = value[0].replace('$$',' ')
-    #             # lang = lang_detect(val)[0]
-    #             lang = lang_detect(val)
-    #             all_lang.add(lang)
-    #             if prediction in final:
-    #                 final[prediction].append({lang: val})
-    #             else:
-    #                 final[prediction] = [{lang: val}]
-    #         elif prediction in ['unique_identifier']:
-    #             if prediction in final:
-    #                 final[prediction].extend(value)
-    #             else:
-    #                 final[prediction] = value
-    #         else:
-    #             for val in value:
-    #                 if '$$' in val:
-    #                     topic = ''
-    #                     for index, text in enumerate(val.split('$$')):
-    #                         text = text.replace('$$', ' ')
-    #                         if len(str(text).split()) > 2:
-    #                             text = ' '.join((topic, text)).strip()
-    #                             topic = ''
-    #                             # lang = classify(text)[0]
-    #                             lang = lang_detect(text)
-    #                             all_lang.add(lang)
-    #                             if prediction in final:
-    #                                 final[prediction].append({lang: text})
-    #                             else:
-    #                                 final[prediction] = [{lang: text}]
-    #                         else:
-    #                             topic = ' '.join((topic, text)).strip()
-    #                             if index == len(val.split('$$')) - 1:
-    #                                 # lang = lang_detect(topic)[0]
-    #                                 lang = lang_detect(topic)
-    #                                 all_lang.add(lang)
-    #                                 if prediction in final:
-    #                                     final[prediction].append({lang: topic})
-    #                                 else:
-    #                                     final[prediction] = [{lang: topic}]
-    #                                 topic = ''
-    #                             else:
-    #                                 pass
-    #                 else:
-    #                     # lang = classify(value[0])[0]
-    #                     lang = lang_detect(value[0])
-    #                     all_lang.add(lang)
-    #                     if prediction in final:
-    #                         final[prediction].append({lang: val})
-    #                     else:
-    #                         final[prediction] = [{lang: val}]
-    #     # if 'unique_identifier' in final:
-    #     #     print('checking unique identifiers')
-    #     #     print(final['unique_identifier'])
-    #     #     for key, identifier in self.regex_parsers_generator(final['unique_identifier']):
-    #     #         identifier = str(identifier).strip()
-    #     #         print('identifier--->',identifier)
-    #     #         if identifier:
-    #     #             final[key] = [identifier]
-    #     #     final.pop('unique_identifier',None)
-    #     if 'unique_identifier' in final:
-    #         identifier_clear = []
-    #         for identifier in final['unique_identifier']:
-    #             identifier_clear.append(identifier.replace('$$',' '))
-    #         final['unique_identifier'] = identifier_clear
-    #     if 'None' in final:
-    #         final.pop('None',None)
-    #     final = {**{'status': 1,'language': list(all_lang),'file_name': [file_name]},**final}
-    #     self.validation(final)
-    #     return final
