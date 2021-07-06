90af6db7eeeea48dfb1fd9e02b199667f7a7d6c4
vj-kanagaraj <Amma@867777>
2021-07-06 10:52:04 +0530

    GM data cleaning and multi language issue fix

--- doc_extractor/extractor/General_mills_processing.py
+++ doc_extractor/extractor/General_mills_processing.py
@@ -3,6 +3,19 @@ from bs4 import BeautifulSoup
 from functools import partial
 from bidi.algorithm import get_display
 from .excel_processing import *
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+# GM_HD_model_dataset = r"/Users/VIJAYKANAGARAJ/PycharmProjects/Schawk_document_xml/Dataset/GM_HD_headers_dataset.xlsx"
+# GM_HD_model_location = r"/Users/VIJAYKANAGARAJ/PycharmProjects/Schawk_document_xml/Dataset/GM_HD_model.pkl"
+# ferrero_model_location = r"/Users/VIJAYKANAGARAJ/PycharmProjects/Schawk_document_xml/Dataset/ferrero_header_model.pkl"
+
+# ip_docx = r"/Users/VIJAYKANAGARAJ/PycharmProjects/Schawk_document_xml/Dataset/HD_M7 Belgian Chocolate Hazelnut 100ml_PS 8261249 ACS0220_kl1.docx"
+# ip_docx = r"/Users/VIJAYKANAGARAJ/PycharmProjects/Schawk_document_xml/Dataset/HD_M7 Dark Chocolate & Caramelized Almonds 100ml_PS 8261255 ACS0220_kl1.docx"
+# ip_docx = r"/Users/VIJAYKANAGARAJ/PycharmProjects/Schawk_document_xml/Dataset/HD_ P2 Dark Chocolate & Caramelized Almond 460ML_PS 8251496 ACS0620_KL1.docx"
+
+
+header_memory = ""
 
 def docx_to_html(file):
     print('entering docx to html')
@@ -45,7 +58,8 @@ def docx_to_table(input_file):
             else:
                 if row_values:
                     df = pd.DataFrame(row_values)
-                    if df.shape[0] < 15:
+                    # if df.shape[0] < 15:
+                    if df.shape[0] < 10:
                         print('---' * 10)
                         yield row_values
 
@@ -119,12 +133,26 @@ def text_preprocessing(text):
     text = re.sub(r"\[.*\]","",text)
     return text
 
-def is_nutrition_table(table : list) -> str:
-    nutrition_table_detector = partial(classifier,griesson_model_location)
-    table_text = " ".join([text.strip() for list in table for text in list])
-    # output = base('general', model_location).prediction(get_display(table_text))['output']
-    output = nutrition_table_detector(table_text)['output']
-    return output
+# def is_nutrition_table(table : list) -> str:
+#     nutrition_table_detector = partial(classifier,griesson_model_location)
+#     table_text = " ".join([text.strip() for list in table for text in list])
+#     # output = base('general', model_location).prediction(get_display(table_text))['output']
+#     output = nutrition_table_detector(table_text)['output']
+#     return output
+
+def is_nutrition_table_or_not(text):
+    table_check = ['nutrition information', 'nutrition information typical values', 'nutrition declaration','Valeurs nutritionnelles moyennes']
+    similarity = 0
+    if isinstance(text,str):
+        similarity = cosine_similarity(laser.embed_sentences(text, lang='en'),
+                                       laser.embed_sentences(table_check, lang='en').mean(0).reshape(1, 1024))[0][0]
+        # print('*******' * 5)
+        print(text, '----->', similarity)
+        # print('*******' * 5)
+        if similarity > 0.80:
+            return True
+        else:
+            return False
 
 # def nutrition_table_processing_old(table:list) -> dict:
 #     nutrition_dict = {}
@@ -162,7 +190,8 @@ def nutrition_table_processing(table:list) -> dict:
     df = pd.DataFrame(table)
     rows, columns = df.shape
     # print('inside nutrition table processing')
-    if 'nutrition information' in str(df[0][0]).lower() and "rdt" in str(df[0][0]).lower():
+    # if 'nutrition information' in str(df[0][0]).lower() and "rdt" in str(df[0][0]).lower():
+    if is_nutrition_table_or_not(df[0][0]):
         print('nutrition_table_found')
         for column in range(columns)[:1]:
             for row in range(rows):
@@ -200,39 +229,57 @@ def nutrition_table_processing(table:list) -> dict:
         return  nutrition_dict
 
 def normal_table_processing(table:list) -> dict:
-    # print(table)
+    global header_memory
+    print('inside normal table processing')
     normal_dict = {}
     df = pd.DataFrame(table)
+    print(df)
     rows, columns = df.shape
     if columns > 1 and columns <= 10:
         for column in range(columns)[:1]:
             for row in range(rows):
                 # print(df[column][row])
                 header = str(df[column][row])
+                if re.search(r"^\(.*\)$",header.strip()) and header_memory and header_memory != 'None':
+                    header = header_memory
+                    print(header,'------->',df)
                 header_cleaned = re.sub(r"\(.*\)","",header).strip()
                 normal_detection = partial(GM_header_classifier,GM_HD_model_location,GM_HD_model_dataset)
                 if header_cleaned:
                     output = normal_detection(get_display(header_cleaned))
                     output_class = output['output']
+                    header_memory = header
                     probability = output['probability']
                     if output_class and probability > 0.90:
                         print(f'{header_cleaned}-----{output_class}-------{probability}')
-                        if output_class == "NET_CONTENT_STATEMENT":
-                            print(table)
-                            print('----' * 5)
+                        # if output_class == "NET_CONTENT_STATEMENT":
+                        #     print(table)
+                        #     print('----' * 5)
                         if str(df[column+1][row]).strip():
-                            if output_class in normal_dict:
-                                normal_dict[output_class].append({'en': df[column+1][row]})
-                            else:
-                                normal_dict[output_class] = [{'en': df[column + 1][row]}]
+                            value = df[column+1][row]
+                            if isinstance(value,str):
+                                value = str(df[column+1][row]).strip()
+                                value = text_cleaning(value).strip()
+                                if value:
+                                    try:
+                                        lang = lang_detect(value)
+                                    except:
+                                        lang = classify(value)[0]
+                                    if output_class in normal_dict:
+                                        normal_dict[output_class].append({lang: value})
+                                    else:
+                                        normal_dict[output_class] = [{lang: value}]
     return normal_dict
 
 def main(input_docx):
     final_dict = {}
     nutrition_final_dict = {}
     for table in docx_to_table(input_docx):
-        output = is_nutrition_table(table)
-        if output == "Nutrition":              # Nutrition table processing
+        # output = is_nutrition_table(table)
+        print('first_object------->',table[0][0])
+        # output = is_nutrition_table_or_not(table[0][0])
+        # if output == "Nutrition":              # Nutrition table processing
+        if is_nutrition_table_or_not(table[0][0]):              # Nutrition table processing
             nutrition_information = nutrition_table_processing(table)
             print(nutrition_information)
             if nutrition_information:
@@ -260,3 +307,18 @@ def main(input_docx):
         final_cleaned_dict[category] = list({frozenset(list_element.items()) : list_element for list_element in value_list}.values())
     # print('after cleaning---->',final_cleaned_dict)
     return {**nutrition_final_dict,**final_cleaned_dict}
+
+def text_cleaning(text):
+    unwanted_text = ['NA','N/A','English Back Translation:','English Back Translation:n/a','English Back Translation:NA',
+                     'new EU recycling logo.PNG< a >','English Back Translation:see english sentence','English Back Translation:see french field',
+                     'Green dot registered.png< a >',' new EU recycling logo.PNG< a >','English Back Translation:see lead country','English Back Translation:See artwork',
+                     'G17 Chocolate 2047032[1]-MGO-G531997A-2','NFP HD CHOCOLATE (MC)','M15 Chocolate - Nutri Info','GM logo.png< a >',
+                     'tbc*','address^','icone vegetarienne.PNG< a >','*only new logo eco emballage in yellow box*','*only new logo eco emballage with following text in yellow box*',
+                     'new EU recycling logo.PNG< a >','OR (if lack of space )','NA.bmp< a >','*Note: website already in the Generic English Information',
+                    ]
+
+    text = re.sub(r"\^.*\^","",text)
+    for text_pattern in unwanted_text:
+        # text = re.sub(r"{}".format(text_pattern),'',text)
+        text = text.replace(text_pattern,'')
+    return text
